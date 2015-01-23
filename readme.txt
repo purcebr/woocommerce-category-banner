aca6c564e9c59b2ed7af7430cd5fafbe1dc73db4
Bryan Purcell <purcebr@gmail.com>
2015-01-22 17:10:43 -0800

    updating to 1.1.2

--- readme.txt
+++ readme.txt
@@ -2,9 +2,9 @@
 Contributors: wpbackoffice
 Tags: woocommerce, category, product, banner, image, archive, shop
 Donate Link: http://wpbackoffice.com/plugins/woocommerce-category-banner/
-Requires at least: 2.3
-Tested up to: 3.9
-Stable tag: 1.1.0
+Requires at least: 3.5
+Tested up to: 4.0.1
+Stable tag: 1.1.2
 License: GPLv2
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
@@ -18,6 +18,7 @@ Features:
 
 * No configuration needed, just install and start using
 * Supports an image for each category
+* Custom product category positions with a template tag for including the image in a custom area in your custom product category template.
 * Support a link for each category / banner image
 
 Getting Started:
@@ -27,6 +28,10 @@ Getting Started:
 3. Paste the url of the banner you'd like to use into the "Banner Image Url" field
 4. That's it, your banner should now be displaying on your category archive page.
 5. Remove image by deleting the url from the edit category page.
+
+You can hide the automatic image placement by unchecking "Automatically insert banner above main content"
+
+You can then place the template tag wcb_show_category_banner() in your custom category template to customize the position of the image in your markup. Note - this tag must be placed on category templates - not product templates.
 
 [Plugin's Official Documentation and Support Page](http://wpbackoffice.com/plugins/woocommerce-category-banner/)
 
@@ -50,6 +55,11 @@ Manual Installation
 
 == Changelog ==
 
+= 1.1.2 =
+* Bug fix, notice was appearing with WP DEBUG set to true
+
+= 1.1.1 =
+* Adding Template tag for custom banner position for custom product category templates.
 
 = 1.1.0 =
 * Added image upload button
--- woocommerce-category-banner.php
+++ woocommerce-category-banner.php
@@ -3,7 +3,7 @@
 Plugin Name: WooCommerce Category Banner
 Plugin URI: http://www.wpbackoffice.com/plugins/woocommerce-category-banner/
 Description: Place a custom banner and link at the top of your product category pages. Easily update the image through your product category edit page.
-Version: 1.1.0
+Version: 1.1.2
 Author: WP BackOffice
 Author URI: http://www.wpbackoffice.com
 */ 
@@ -15,7 +15,10 @@ if ( ! class_exists( 'WCB_Category_Banner' ) ) :
 class WCB_Category_Banner {
 	
 	public function __construct() {
-		
+
+		global $woocommerce;
+		global $wp_query;
+
 		// Add Scripts and styles		
 		add_action( 'admin_enqueue_scripts', array( $this, 'admin_scripts_and_styles' ) );
 		
@@ -25,8 +28,9 @@ class WCB_Category_Banner {
 		// Save the changes made on the product category taxonomy 
 		add_action( 'edited_product_cat', array( $this, 'wcb_product_cat_save_taxonomy_custom_fields'), 10, 2 );  
 
-		// Add a banner image based on category taxonomy image
+		// Add a banner image based on category taxonomy image, only if it's set to auto show (default)
 		add_action( 'woocommerce_before_main_content', array( $this, 'wcb_show_category_banner'), 30 );
+
 	}
 
 	/*
@@ -76,7 +80,14 @@ class WCB_Category_Banner {
     		$banner_link = $term_meta['banner_link'];
     	else 
     	    $banner_link = null;
-    	    
+    	  
+    	// Get auto display setting.
+		if ( (isset( $term_meta['auto_display_banner']) && $term_meta['auto_display_banner'] == 'on') || !isset( $term_meta['auto_display_banner']) ) {
+			$auto_display_banner = true;
+		} else {
+			$auto_display_banner = false;
+		}
+
 	    ?>  
 	    
 		<tr class="form-field banner_url_form_field">  
@@ -109,6 +120,17 @@ class WCB_Category_Banner {
 		    	</fieldset>
 			</td>  
 		</tr> 
+		<tr class="form-field auto_display_banner">  
+		    <th scope="row" valign="top">  
+		        <label for="auto_display_banner"><?php _e('Automatically insert banner above main content'); ?></label>  
+		    </th>  
+		    <td>  
+		    	<fieldset>
+		    	<input name="term_meta[auto_display_banner]" type="checkbox" value="on" class="auto_display_banner" <?php if($auto_display_banner) echo " checked "; ?>/>
+				<label class="auto_display_banner_label" for="auto_display_banner"><em>If you want to display the banner in a custom spot on your category page, you can deselect this checkbox and use the wcb_show_category_banner() in your category template to dictate where it will appear.</em></label>		
+		    	</fieldset>
+			</td>  
+		</tr> 
 		  
 	<?php  
 	}
@@ -119,11 +141,16 @@ class WCB_Category_Banner {
 	    if ( isset( $_POST['term_meta'] ) ) {  
 	        $t_id = $term_id;  
 	        $term_meta = get_option( "taxonomy_term_$t_id" );  
-	        $cat_keys = array_keys( $_POST['term_meta'] );  
+	        $posted_term_meta = $_POST['term_meta'];
 
-	            foreach ( $cat_keys as $key ){  
-	            if ( isset( $_POST['term_meta'][$key] ) ){  
-	                $term_meta[$key] = $_POST['term_meta'][$key];  
+	        if(!isset($posted_term_meta['auto_display_banner']))
+	        	$posted_term_meta['auto_display_banner'] = 'off';
+
+	        $cat_keys = array_keys( $posted_term_meta );  
+
+	        foreach ( $cat_keys as $key ){  
+	            if ( isset( $posted_term_meta[$key] ) ){  
+	                $term_meta[$key] = $posted_term_meta[$key];  
 	            }  
 	        }  
 	        //save the option array  
@@ -135,38 +162,47 @@ class WCB_Category_Banner {
 	public function wcb_show_category_banner() {
 		global $woocommerce;
 		global $wp_query;
-		
+
 		// Make sure this is a product category page
 		if ( is_product_category() ) {
-			
 			$cat_id = $wp_query->queried_object->term_id;
-			$term_options = get_option( "taxonomy_term_$cat_id" ); 
+
+			$term_options = get_option( "taxonomy_term_$cat_id" );
+
+			if((isset($term_options['auto_display_banner']) && $term_options['auto_display_banner'] == 'on') || !isset($term_options['auto_display_banner'])) {
+				// Get the banner image id
+				if ( $term_options['banner_url_id'] != '' )
+					$url = wp_get_attachment_url( $term_options['banner_url_id'] ); 
+
+				// Exit if the image url doesn't exist
+				if ( !isset( $url ) or $url == false )
+					return;
+
+				// Get the banner link if it exists
+				if ( $term_options['banner_link'] != '' )
+					$link = $term_options['banner_link'];
+
+				// Print Output
+				if ( isset( $link ) )
+					echo "<a href='" . $link . "'>"; 
 			
-			// Ge the banner image id
-			if ( $term_options['banner_url_id'] != '' )
-				$url = wp_get_attachment_url( $term_options['banner_url_id'] ); 
+				if ( $url != false ) 
+					echo "<img src='" . $url . "' class='category_banner_image' />";
+			
+				if ( isset( $link ) )
+					echo "</a>";
 
-			// Exit if the image url doesn't exist
-			if ( !isset( $url ) or $url == false )
-				return;
-
-			// Get the banner link if it exists
-			if ( $term_options['banner_link'] != '' )
-				$link = $term_options['banner_link'];
-
-			// Print Output
-			if ( isset( $link ) )
-				echo "<a href='" . $link . "'>"; 
-		
-			if ( $url != false ) 
-				echo "<img src='" . $url . "' class='category_banner_image' />";
-		
-			if ( isset( $link ) )
-				echo "</a>";
+			}
 		}
-	}  
+	}
 }
 
 endif;
 
-new WCB_Category_Banner();
+$WCB_Category_Banner = new WCB_Category_Banner();
+
+//Shortcode function for displaying banner.
+function wcb_show_category_banner() {
+	global $WCB_Category_Banner;
+	$WCB_Category_Banner->wcb_show_category_banner(); //disable the only show for category tag.
+}
